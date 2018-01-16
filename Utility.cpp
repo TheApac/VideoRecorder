47f67e4330dbe94f3fc42f97f6033ab7a116d15d
Alexandre <apac.alexcuevas@gmail.com>
2018-01-16 14:02:00 +0100

    Ajouts de commentaires sur le fichier Utility

--- Utility.cpp
+++ Utility.cpp
@@ -37,11 +37,10 @@ void deamonize() {
     if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
-    umask(0);
-    chdir("/");
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--) {
-        close(x);
+    umask(0); //remove every right to the process
+    chdir("/"); // change directory to root
+    for (int x = sysconf(_SC_OPEN_MAX); x >= 0; x--) { 
+        close(x); //close every system file possibly opened by the process
     }
 }
 
@@ -51,11 +50,11 @@ void createDirectoryVideos(string rootDirectory) {
     struct tm * now = localtime(& t); //get local time
     string month = to_string(now->tm_mon + 1);
     if (strlen(month.c_str()) < 2) {
-        month = "0" + month;
+        month = "0" + month; // if month < 10 we add a 0 in front of the number
     }
     string day = to_string(now->tm_mday);
     if (strlen(day.c_str()) < 2) {
-        day = "0" + day;
+        day = "0" + day; // if day < 10 we add a 0 in front of the number
     }
     string date = rootDirectory + "/" + std::to_string(now->tm_year + 1900) + "." + month + "." + day; //get current date
     string hour = date + "/H" + to_string(now->tm_hour); //get current hour
@@ -84,21 +83,21 @@ void removeContentOfDirectory(string path, bool exact) {
     time_t t = time(0); // get time now
     struct tm * now = localtime(& t); //get local time
     DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir(path.c_str())) != NULL) {
-        while ((ent = readdir(dir)) != NULL) {
-            if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+    struct dirent *ent; // will be used to store every entity in the directory
+    if ((dir = opendir(path.c_str())) != NULL) { // try to open the directory
+        while ((ent = readdir(dir)) != NULL) { // iterate through its content
+            if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) { //Do not iterate on current and parent directories
                 continue;
             }
-            string a = ent->d_name;
-            if (ent->d_type == DT_DIR && !exact) {
+            string entryName = ent->d_name; // we store the entry name in a string
+            if (ent->d_type == DT_DIR && !exact) { // if the date is higher than nbDays remove everything in every subdirectories
                 removeContentOfDirectory(path + "/" + ent->d_name, true);
-                printf("%s/%s\n",path.c_str(),ent->d_name);
-            } else if (ent->d_type == DT_DIR && exact && now->tm_hour > atoi(a.substr(a.size() - 2).c_str())) {
+                printf("%s/%s\n",path.c_str(),ent->d_name); // remove the subdirectory
+            } else if (ent->d_type == DT_DIR && exact && now->tm_hour > atoi(entryName.substr(entryName.size() - 2).c_str())) { // else only remove when the hour indicated is lower than current hour
                 removeContentOfDirectory(path + "/" + ent->d_name, true);
-                printf("%s/%s\n",path.c_str(),ent->d_name);
+                printf("%s/%s\n",path.c_str(),ent->d_name); // remove the subdirectory
             } else if (ent->d_type != DT_DIR) {
-                printf("%s/%s\n", path.c_str(),ent->d_name);
+                printf("%s/%s\n", path.c_str(),ent->d_name); // if it's not a directory, remove it
             }
         }
     } else {
@@ -120,12 +119,12 @@ int removeOldFile(int nbDays, string path) {
             // check if the directory is older than nbDays
             if (timeSinceDate(ent->d_name) > nbDays) {
                 string folderToRemove = path + "/" + ent->d_name; //absolute path to directory
-                removeContentOfDirectory(folderToRemove, false);
+                removeContentOfDirectory(folderToRemove, false); // empty the directory
                 printf("%s\n", folderToRemove.c_str()); // remove the directory
             }
-            if (timeSinceDate(ent->d_name) == nbDays) {
+            if (timeSinceDate(ent->d_name) == nbDays) { // if date exactly equal to nbDays, we don't delete files where hour is higher than current hour
                 string folderToRemove = path + "/" + ent->d_name; //absolute path to directory
-                removeContentOfDirectory(folderToRemove, true);
+                removeContentOfDirectory(folderToRemove, true); // remove files that need to be removed
             }
         }
         closedir(dir);
