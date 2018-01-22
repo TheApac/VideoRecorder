9625b4332803c78652796916a9725afa376c768d
Alexandre <apac.alexcuevas@gmail.com>
2018-01-22 15:56:50 +0100

    Création fonction d'enregistrement

--- Camera.cpp
+++ Camera.cpp
@@ -25,11 +25,11 @@ Camera::Camera(string str) {
     this->ID = 2;
     this->directory = str;
     //createDirectoryVideos(this->directory);
-    while (1) {
+    //while (1) {
         VideoCapture inputVideo("rtsp://" + this->url);
         if (!inputVideo.isOpened()) {
             printf("Couldn't connect to camera\n");
-            break;
+            //break;
         }
         const string NAME = "testRecordN01.avi";
         int ex = static_cast<int> (inputVideo.get(CV_CAP_PROP_FOURCC));
@@ -37,19 +37,31 @@ Camera::Camera(string str) {
                 (int) inputVideo.get(CV_CAP_PROP_FRAME_HEIGHT));
 
         VideoWriter outputVideo; // Open the output
-        outputVideo.open(NAME, ex, inputVideo.get(CV_CAP_PROP_FPS), size, true);
-        if (!outputVideo.isOpened()) {
-            cout << "Could not open the output video for write: " << this->url << endl;
-            break;
-        }
-        Mat src;
-        time_t t = time(0);
-        long int secondsToStop = time(&t) + 30;
-        while (time(&t) < secondsToStop) { //Show the image captured in the window and repeat
-            inputVideo >> src; // read
-            outputVideo.write(src);
-        }
-    }
+        //outputVideo.open(NAME, CV_FOURCC('M','P','4','2'), inputVideo.get(CV_CAP_PROP_FPS), size, true);
+//        if (!outputVideo.isOpened()) {
+//            cout << "Could not open the output video for write: " << this->url << endl;
+//            break;
+//        }
+            int fourcc = static_cast<int>(inputVideo.get(CV_CAP_PROP_FOURCC));
+
+        char FOURCC_STR[] = {
+        (char)(fourcc & 0XFF)
+        , (char)((fourcc & 0XFF00) >> 8)
+        , (char)((fourcc & 0XFF0000) >> 16)
+        , (char)((fourcc & 0XFF000000) >> 24)
+        , 0
+    };
+    std::cout << "FOURCC is '" << FOURCC_STR << "'\n";
+//        Mat src;
+//        time_t t = time(0);
+//        long int secondsToStop = time(&t) + 30;
+//        while (time(&t) < secondsToStop) { //Show the image captured in the window and repeat
+//            //inputVideo >> src; // read
+//            //outputVideo.write(src);
+//        }
+//        outputVideo.release();
+//        inputVideo.release();
+    //}
 }
 
 Camera::~Camera() {
